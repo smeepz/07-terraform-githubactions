4fdc649292ee4659b4ad2a3469f5a331aa537c41
glennbech <glenn.bech@gmail.com>
2021-10-12 01:52:17 +0200

    Infra

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Devops4Life!
+          Edit <code>src/App.js</code> Save and reload !
         </p>
         <a
           className="App-link"
