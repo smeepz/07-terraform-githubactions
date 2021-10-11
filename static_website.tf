4f8928d44e009f69ef0b9ba63d0a4cf55c11e73f
glennbech <glenn.bech@gmail.com>
2021-10-11 23:09:10 +0200

    fmt

--- static_website.tf
+++ static_website.tf
@@ -1,8 +1,8 @@
 module "static-site" {
-  source  = "telia-oss/static-site/aws"
-  version = "3.0.0"
+  source            = "telia-oss/static-site/aws"
+  version           = "3.0.0"
   bucket_versioning = true
-  hosted_zone_name = "thecloudcollege.com"
-  name_prefix      = "glenn"
-  site_name        = "glenn.thecloudcollege.com"
+  hosted_zone_name  = "thecloudcollege.com"
+  name_prefix       = "glenn"
+  site_name         = "glenn.thecloudcollege.com"
 }
