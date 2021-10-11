6b8ab90785a018a225229b6d8bb95e1dd88e4c05
glennbech <glenn.bech@gmail.com>
2021-10-11 23:10:47 +0200

    disable bucket versioning

--- static_website.tf
+++ static_website.tf
@@ -1,7 +1,7 @@
 module "static-site" {
   source            = "telia-oss/static-site/aws"
   version           = "3.0.0"
-  bucket_versioning = true
+  bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
   name_prefix       = "glenn"
   site_name         = "glenn.thecloudcollege.com"
