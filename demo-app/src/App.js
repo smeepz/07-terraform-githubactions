aa738810e2e8a76cea0c04784ff369faf7106aae
EC2 Default User <ec2-user@ip-172-31-13-118.eu-north-1.compute.internal>
2021-10-12 09:27:55 +0000

    added meme

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> DevOpsForLife !
+          Edit <code>src/App.js</code> BlackOpsForLife !
         </p>
         <a
           className="App-link"
